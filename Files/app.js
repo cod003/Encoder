4bfe4849337e4aeafec6318a49e1e6d11dcb182f
Isa Ali <88540620+Destiny-003@users.noreply.github.com>
2022-02-09 10:00:59 -0800

    Update app.js

--- Files/app.js
+++ Files/app.js
@@ -25,23 +25,6 @@ function chbackg() {
     })
 }
 }
-
-
-
-
-
-
-// The btoa() method encodes a string in base-64.
-// btoa() : يتم استخدامها لتشفير بيانات النصية إلى تشفير بنظام القاعدة64
-// ----------------------------------------------------------------------------
-// This method uses the "A-Z", "a-z", "0-9", "+", "/" and "=" characters to encode the string. 
-// هذا عنصر يستخدم احرف اللغة الإنجليزية , الصغيرة والكبيرة , الأرقام و علامة 
-// +  و  /  و =
-// عندما يقوم بتشفير النص
-//------------------------------------------------------------------------------
-// Tip: Use the atob() method to decode a base-64 encoded string.
-// atob() : يتم استخدام هذا العنصر لفك تشفير نص مشفر بنظام قاعدة64
-
 let text = "Destiny Encoder";
 let encode_text = window.btoa(text);
 
